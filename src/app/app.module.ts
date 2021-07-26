c7f3a45aa5375989a61dce931954eb56ebbcffca
BLADE <abdirahman.ahmed@student.moringaschool.com>
2021-07-26 16:09:16 +0300

    mAdd search bar and submit button

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GithubAppComponent } from './github-app/github-app.component';
+import { GithubProfileComponent } from './github-profile/github-profile.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GithubAppComponent,
+    GithubProfileComponent
   ],
   imports: [
     BrowserModule,
